00526b595dee47855db90140a9642c5d64d71285
tobytaken <tobiassteinhaeusler@gmail.com>
2023-10-28 11:31:35 +0200

    Add animations, hover, styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,24 @@
-// import * as MainNav from './lib/mainnav.js';
-// window.MainNav = MainNav;
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Script loaded!');
+  document.querySelector('#hamburger').addEventListener('click', toggleBurger);
 });
 
 const toggleBurger = () => {
   const nav = document.querySelector('nav');
-  nav.classList.toggle('hidden');
+
+  if (!nav) return;
+
+  if (!nav.classList.contains('hidden')) {
+    setTimeout(() => {
+      nav.classList.toggle('hidden');
+      nav.classList.remove('slide-out');
+    }, 300);
+    nav.classList.add('slide-out');
+  } else {
+    setTimeout(() => {
+      nav.classList.remove('slide-in');
+    }, 300);
+    nav.classList.add('slide-in');
+    nav.classList.toggle('hidden');
+  }
 }
